fabc938c05f694ad53b166c4e497c508f5f9e79c
Andres <andrespiraneque7@gmail.com>
2026-09-03 10:38:37 -0500

    mi primer commit

--- operaciones.py
+++ operaciones.py
@@ -2,12 +2,12 @@ numero1,numero2 = map(int,input("Ingrese dos numeros: ").split())
 
 suma = numero1 + numero2
 resta = numero1 - numero2
-mul90tiplicacion = numero1 * numero2
+multiplicacion = numero1 * numero2
 division = numero1 / numero2
+promedio = (numero1 + numero2)/2
 
 print("Resultado de la suma: ", suma)
 print("Resultado de la resta: ", resta)
 print("Resultado de la multiplicacion: ", multiplicacion)
-print("Resultado de la division: ", division)
-
-    
+print("Resultado de la division: ", division)  
+print("El promedio de los numeros es: ", promedio)
